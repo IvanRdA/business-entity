06e3bff96524312d46c31628fe66e49a20d84e94
Ivan Rodriguez <ivan@ivanrda.es>
2023-08-20 21:52:41 +0200

    Added the basic methods for controllers

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -1,3 +1,72 @@
-const adminController = {};
+const adminController = {
+  // CLIENTS
+  createClient: () => {},
+  updateClient: (id) => {},
+  deleteClient: (id) => {},
+  getAllClients: () => {},
+  getSingleClient: (id) => {},
+
+  // EXPENSES
+  createExpense: () => {},
+  updateExpense: (id) => {},
+  deleteExpense: (id) => {},
+  getAllExpenses: () => {},
+  getSingleExpenses: (id) => {},
+
+  // ORDERS
+  createOrder: () => {},
+  updateOrder: (id) => {},
+  deleteOrder: (id) => {},
+  getAllOrders: () => {},
+  getSingleOrder: (id) => {},
+
+  // PRODUCTS
+  createProduct: () => {},
+  updateProduct: (id) => {},
+  deleteProduct: (id) => {},
+  getAllProducts: () => {},
+  getSingleProduct: (id) => {},
+
+  // SALES
+  createSale: () => {},
+  updateSale: (id) => {},
+  deleteSale: (id) => {},
+  getAllSales: () => {},
+  getSingleSale: (id) => {},
+
+  // SCHEDULES
+  createSchedule: () => {},
+  updateSchedule: (id) => {},
+  deleteSchedule: (id) => {},
+  getAllSchedules: () => {},
+  getSingleSchedule: (id) => {},
+
+  // SETTINGS
+  updateSettings: () => {},
+  getSettings: () => {},
+
+  // SHOPS
+  createShop: () => {},
+  updateShop: (id) => {},
+  deleteShop: (id) => {},
+  getAllShops: () => {},
+  getSingleShop: (id) => {},
+
+  // SUPPLIERS
+  createSupplier: () => {},
+  updateSupplier: (id) => {},
+  deleteSupplier: (id) => {},
+  getAllSuppliers: () => {},
+  getSingleSupplier: (id) => {},
+
+  // WORKERS
+  createWorker: () => {},
+  updateWorker: (id) => {},
+  deleteWorker: (id) => {},
+  getAllWorkers: () => {},
+  getSingleWorker: () => {},
+
+  // DASHBOARD
+};
 
 module.exports = adminController;
---
+++ backend/src/controllers/login.controller.js
@@ -0,0 +1,5 @@
+const loginController = {
+  checkCredentials: () => {},
+};
+
+module.exports = loginController;
