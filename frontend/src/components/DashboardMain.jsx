b6aa0824ddc8f857a339d7e6ce2913846a4a9e0b
Ivan Rodriguez <ivan@ivanrda.es>
2023-08-21 15:04:40 +0200

    Frontend files created

---
+++ frontend/src/components/DashboardMain.jsx
@@ -0,0 +1,10 @@
+export default function Dashboard(){
+    return(
+        <>
+        <div className="text-center text-[--blackColor] max-w-[80vw] w-[80vw] m-auto bg-gradient-to-r from-[#DA172A] to-[#910014] min-h-screen grid items-center">
+            <h1>DASHBOARD</h1>
+            <h3>Algunos graficos aqui</h3>
+        </div>
+        </>
+    )
+}
