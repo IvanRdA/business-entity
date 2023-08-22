6dd7b31601a019f2756a556ce7fe949489d4a809
Ivan Rodriguez <ivan@ivanrda.es>
2023-08-22 22:53:44 +0200

    Logic refactoring for input handling

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,46 +1,57 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home(){
     // SETTING STATES FOR THE EMAIL AND PASSWORD INPUTS
     const [mail, setMail] = useState('');
     const [pwd, setPwd] = useState('');
+    const [flags, setFlags] = useState({
+      mailFlag: false,
+      pwdFlag: false
+    })
 
-    const [reqBody, setReqBody] = useState({});
+    const mailRegExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const pwdRegExp = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{6,20}).*$/;
 
-    
-    // SETTING AN EFFECT STATUS FOR THE VALIDATIONS OF THE INPUTS
     useEffect(() => {
-      const emailInput = document.getElementById('email');
-      const pwdInput = document.getElementById('pwd');
-      const emailSpan = document.getElementById('mail-validation-msg');
-      const pwdSpan = document.getElementById('pwd-validation-msg');
-      const submitBtn = document.getElementById('submit-btn');
-
-
-      const regExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-      if(!regExp.test(mail) && mail){
-        emailInput.classList?.remove('border-lime-600', 'border-4');
-        emailInput.classList.add('border-amber-400', 'border-4', 'disabled');
-        emailSpan.textContent = 'Formato de correo no valido';
-
-      }else if(regExp.test(mail)){
-        emailInput.classList?.remove('border-amber-400', 'border-4', 'disabled');
-        emailInput.classList.add('border-lime-600', 'border-4');
-        emailSpan.textContent = '';
+      // NEED TO CHECK THE ELSE IF STATEMENTS BECAUSE ONCE IT GETS TO TRUE NEVER CHANGE TO FALSE AGAIN. ONCE IT'S SOLVED THE FUNCTIONALITY WILL WORK COMPLETELY
+      if(mailRegExp.test(mail)){
+        console.log(flags)
+        console.log([mail, pwd])
+        setFlags({
+          ...flags,
+          mailFlag: true
+        });
+      }else if(mailRegExp.test(mail) == false){
+        console.log(flags)
+        console.log([mail, pwd])
+          setFlags({
+            ...flags,
+            mailFlag: false
+        });
       }
-    }, [mail, pwd])
 
-    // HANDLELING THE EMAIL AND PASSWORD INPUTS
+      if(pwdRegExp.test(pwd)){
+        setFlags({
+          ...flags,
+          pwdFlag: true
+        });
+      }else if(!pwdRegExp.test(pwd)){
+          setFlags({
+            ...flags,
+            pwdFlag: false
+        });
+      }
+    }, [mail, pwd]);
+
+    // HANDLING THE EMAIL AND PASSWORD INPUTS
     function handleMail(e){
       e.preventDefault();
       setMail(e.target.value);
-      console.log(mail);
     };
     function handlePassword(e){
       e.preventDefault();
       setPwd(e.target.value);
-      console.log(pwd);
-    };
+    }
 
     // VALIDATING CREDENTIALS ON THE BACKEND AND MANAGING THE RESPONSE
     async function loginValidation(e){
@@ -49,17 +60,9 @@ export default function Home(){
 
       if(!regExp.test(mail)){
         alert('Formato de correo incorrecto. Pruebelo de nuevo.');
-        setMail('');
       }else{
-        setReqBody({
-          email: mail
-        });
         alert('Working');
       }
-      
-
-
-
     };
 
     return(
@@ -78,10 +81,10 @@ export default function Home(){
             <div className="flex flex-col">
               <label className="text-[#DA172A]" htmlFor="email">Email</label>
               <input
-                className="w-[100%] py-1 text-[--darkRed]"
+                value={mail}
+                className="w-[100%] py-1 text-[--darkRed] focus:border-blue-500 p-2"
                 type="text"
                 name="email"
-                value={mail}
                 id="email"
                 placeholder="example@example.com"
                 onChange={handleMail}
@@ -91,10 +94,10 @@ export default function Home(){
             <div className="flex flex-col">
               <label className="text-[#DA172A]" htmlFor="password">Password</label>
               <input
-                className="w-[100%] py-1"
+                value={pwd}
+                className="w-[100%] py-1 focus:border-blue-500 p-2"
                 type="password"
                 name="pwd"
-                value={pwd}
                 id="pwd"
                 placeholder="MyPasswordHere"
                 onChange={handlePassword}
@@ -102,18 +105,23 @@ export default function Home(){
               <span className='text-xs text-[--warningColor]' id='pwd-validation-msg'></span>
             </div>
           </div>
-          <input
-            className={
-              reqBody.email //&& reqBody.password 
-              ? "bg-[#910014] hover:bg-[#DA172A] hover:scale-110 text-[#F0F0F0] py-2 px-5 m-5 cursor-pointer ease-out duration-200 rounded-tr-md rounded-bl-md" 
-              : "bg-[--greyColor] text-[--blackColor] py-2 px-5 m-5 rounded-tr-md rounded-bl-md"
-              }
+          {flags.mailFlag && flags.pwdFlag
+          ? <input
+            className= "bg-[#910014] hover:bg-[#DA172A] hover:scale-110 text-[#F0F0F0] py-2 px-5 m-5 cursor-pointer ease-out duration-200 rounded-tr-md rounded-bl-md "
             type="submit"
             name="submit-btn"
             id="submit-btn"
             value="INICIAR SESION"
-            disabled
           />
+          : <input
+          className= "bg-[--greyColor]] text-[--blackColor] py-2 px-5 m-5"
+          type="submit"
+          name="submit-btn"
+          id="submit-btn"
+          value="INICIAR SESION"
+          disabled
+        />
+        }
         </form>
       </div>
     )
